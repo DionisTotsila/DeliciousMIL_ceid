efe277625431a5d48ea0caf4841e7409d4e7985b
DionisTotsila <st1067532@ceid.upatras.gr>
2022-04-27 11:57:23 +0300

    BoW attempt

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,1 +1,10 @@
 import numpy as np
+import pandas as pd
+
+def dat_to_df(name):
+    assert name == "train" or name == "test"
+    # csv is used because rows are seperated by new lines we do not make use of the "," delimiter
+    df = pd.read_csv("../data/" + name + "-data.dat", names=["data"])
+    df["label"] = pd.read_csv("../data/"+ name + "-label.dat", header=None)
+    return df
+

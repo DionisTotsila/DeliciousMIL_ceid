98659939e1982b6ac0c8fc7586425ceb8929bbaa
DionisTotsila <st1067532@ceid.upatras.gr>
2022-05-02 12:04:50 +0300

    Scaling

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re 
+from sklearn.preprocessing import MinMaxScaler
 
 # read .dat dataset and create pandas dataframe
 def dat_to_df(name):
@@ -16,11 +17,19 @@ def bow(row):
     # create BoW dict
     # zeros intialize use range as keys
     diction = dict.fromkeys((str(i) for i in range(8520)), 0)
-    #iterate through input and increment corresponding dict value (ignoring <num> since it does not provide any information)
+    
+    #remove <num> substrings from text
+    #print(text)
+    text = re.sub('<[0-9]+>',"",text)
+    #print(text)
+    # fix double spaces occuring
+    text = ' '.join(text.split())
+    
+    #iterate through every number
     for numbers in re.findall(re.compile('[0-9]+'), text):
+        
         diction[numbers] = diction[numbers] + 1
-    return (np.array([list(diction.values())]))
-
+    return (np.array(list(diction.values())))
 
 
 test_df = dat_to_df("test")
@@ -30,6 +39,27 @@ test_df['bow'] = test_df.apply(bow, axis = 1)
 train_df['bow'] = train_df.apply(bow, axis = 1)
 
 print(test_df)
+print(train_df)
 
-# TO DO 
+
 # Feature scaling
+
+def scale(df):
+    temp = df['bow'].values
+    temp = np.array(temp.tolist())
+    # init scaler
+    scaler = MinMaxScaler().fit(temp)
+    # scale data
+    X_scaled = scaler.transform(temp)
+    # create row with scaled data
+    df['scaled'] = pd.DataFrame(((x,) for x in X_scaled), columns = ['scaled'])
+    return df
+
+
+test_df = scale(test_df)
+train_df = scale(train_df)
+
+# save  new datasets
+test_df.to_csv("data/test_df.csv")
+train_df.to_csv("data/train_df.csv")
+
